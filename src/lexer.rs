3e12d6cfadee02c05868e7815527f290963b00c3
David L. Inman <davidlevere@gmail.com>
2019-01-08 22:40:54 -0500

    inc prog

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,13 +1,26 @@
 
 use super::data::Token;
 
+enum AccumState {
+    Ja,
+}
+
+struct TokenAccum {
+    tokens : Vec<Token>,
+    state : AccumState,
+}
+
+impl TokenAccum {
+}
+
+fn lex_next(ta : TokenAccum, charIndex : (usize, char)) -> TokenAccum {
+        
+    TokenAccum { tokens: ta.tokens, state: AccumState::Ja }
+}
 
 pub fn lex(text : &str) -> Vec<Token> {
     let words = text.char_indices();
-
-    for x in words {
-        println!( "{:?}", x);
-    }
+    let o = words.fold(TokenAccum{}, lex_next);
 
     vec! [Token::Comma]
 }
